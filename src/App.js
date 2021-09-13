77ecad1a78ba718d9d88448016f5461a5ea9d272
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-09-12 21:23:45 -0300

    Custom Hooks

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ContadorHooks from './components/ContadorHooks';
 import ScrollHooks from './components/ScrollHooks';
 import RelojHooks from './components/RelojHooks';
 import AjaxHooks from './components/AjaxHooks';
+import HooksPersonalizados from './components/HooksPersonalizados';
 function App() {
   return (
     <div className="App">
@@ -68,6 +69,8 @@ function App() {
           <RelojHooks/>
           <hr/>
           <AjaxHooks/>
+          <hr/>
+          <HooksPersonalizados/>
        </section>
       </header>
     </div>
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,45 @@
+//Hook Personalizado
+//Para que React sepa que es un hook personalizado la funcion debe usar lowercamelcase
+import { useState, useEffect } from 'react';
+
+export const useFetch = (url) => {
+    {/*Control datos que ingresan */}
+    const [data, setData] = useState(null);
+    //informacion pendiente, empieza con valor true
+    const [isPending, setIsPending] = useState(true);
+    //Manejo errores
+    const [error, setError] = useState(null);
+
+    //Se ejecuta cuando cambie la variable url
+    useEffect(() => {
+        const getData = async (url) => {
+        try{
+                let res = await fetch(url);
+                //espera la respuesta a la variable fetch
+
+            //si la respuesta es falsa (hubo un error)
+            if(!res.ok){
+                throw{
+                    err:true,
+                    status: res.status,
+                    statusText: !res.statusText ? "Ocurrio un error" : res.statusText,
+                };
+            }
+            //Si existe una petición, la espera y convierte en formato json
+            let data = await res.json();
+
+            //En caso de no existir error, se actualiza el estado
+            setIsPending(false)
+            setData(data);
+            setError({err: false});
+            }catch(err){
+                setIsPending(true);
+                setError(err);
+            }
+        };
+        //Llamado a api
+        getData(url);
+    }, [url]);
+    //Retorna un objeto 
+    return {data, isPending, error};
+}
