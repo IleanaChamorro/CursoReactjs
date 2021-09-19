b8cc2e33d64051fca5d8ad86fdaa8784f3d074c0
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-09-19 13:26:14 -0300

    Frameworks css con React

---
+++ frameworkscss/frameworkscss/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Bulma from './components/Bulma';
+
+function App() {
+  return (
+    <div>
+      <h1>Frameworks CSS con React</h1>
+      {/* <Bootstrap/> */}
+      <Bulma/>
+    </div>
+  );
+}
+
+export default App;
