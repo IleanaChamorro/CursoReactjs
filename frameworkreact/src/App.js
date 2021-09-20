a6c8c328306b5795b1fd4e683fae16ba8c08dcc2
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-09-20 10:52:08 -0300

    Clase 26 - React Bootstrap & Material ui

---
+++ frameworkreact/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MaterialUi from './components/MaterialUi';
+
+function App() {
+  return (
+    <div>
+    <h1>Frameworks CSS con React</h1>
+    {/* <Bootstrap/> */}
+    {/*<Bulma/>*/}
+    {/*<ReactBootstrap/>*/}
+    <MaterialUi/>
+  </div>
+  );
+}
+
+
+export default App;
+
---
+++ frameworkreact/src/components/MaterialUi.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MaterialCard from './MaterialCard';
+
+export default function MaterialUi(){
+    return(
+        <>
+        <h2>Material Ui</h2>
+        <MaterialCard/>
+        </>
+    )
+}
