b07837548013f906f3bf1cf29f0b42ce19485f70
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-09-26 22:15:19 -0300

    Crud app - eliminacion de datos y stylecss

--- crudapp/src/components/CrudApp.js
+++ crudapp/src/components/CrudApp.js
@@ -47,22 +47,37 @@ const CrudApp = () => {
         //Actualizacion db con nueva data
         setDb(nuevaData);
     };
-    const borrarData = (id) => {};
+    const borrarData = (id) => {
+        //Consultar si el usuario esta seguro de borrar 
+        let borrar = window.confirm(
+            `¿Esta seguro de eliminar el registro '${id}' ?`
+        );
+
+        //Accion de Borrar
+        if(borrar){
+            let nuevaData = db.filter(el => el.id !== id);
+            setDb(nuevaData);
+        }else{
+            return;
+        }
+    };
 
     return (
         <div>
             <h2>Crud App</h2>
-            <CrudForm 
-            crearData={crearData}
-            actData={actData}
-            dataAEditar={dataAEditar}
-            setDataAEditar={setDataAEditar}
-            />
-            <CrudTable 
-            data={db}
-            setDataAEditar={setDataAEditar}
-            borrarData={borrarData}
-            />
+            <article className="grid-1-2">
+                <CrudForm 
+                crearData={crearData}
+                actData={actData}
+                dataAEditar={dataAEditar}
+                setDataAEditar={setDataAEditar}
+                />
+                <CrudTable 
+                data={db}
+                setDataAEditar={setDataAEditar}
+                borrarData={borrarData}
+                />
+            </article>
         </div>
     )
 }
