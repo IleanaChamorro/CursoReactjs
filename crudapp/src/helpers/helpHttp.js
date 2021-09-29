d8ee4f3888ec67d8ec4900d1fbabbb756beb67ca
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-09-29 11:20:24 -0300

    Crud api - Helper para peticiones ajax

---
+++ crudapp/src/helpers/helpHttp.js
@@ -0,0 +1,68 @@
+export const helpHttp = () => {
+    //Peticion fetch
+    const fetchPet = (endpoint, options) => {
+        const defaultHeader = {
+            accept: "application/json",
+        };
+
+        //Manejador de Errores
+        const controller = new AbortController();
+        options.signal = controller.signal;
+
+        //Si el usuario en opciones trae metodo, dejar metodo. En caso de no traer nada queda por default GET
+        options.method = options.method || "GET";
+
+        //Si el usuario especifico las cabeceras de esta peticion, se crea un nuevo objeto que mezcla cabeceras por defecto mas las del usuario, cuando no indica "DEFAULT"
+        options.headers = options.headers
+        ? {...defaultHeader, ...options.header} 
+        :  defaultHeader;
+
+        //Peticiones con datos(Conversion objeto a cadena)
+        options.body = JSON.stringify(options.body) || false;
+        //Cuando el body tenga el valor de falso lo elimina
+        if(!options.body) delete options.body;
+
+        console.log(options);
+        //Si despues de 3seg no hay respuesta por parte del servidor, se cancela la peticion 
+        setTimeout(() => controller.abort(), 3000);
+
+        return fetch(endpoint, options)
+        .then((res) => 
+            res.ok
+            ? res.json()
+            : Promise.reject({
+                err:true,
+                status: res.status || "00",
+                statusText: res.statusText || "Ocurrio un error",
+            })
+        )
+        .catch((err) => err);
+    };
+
+    //metodo get
+    const get = (url, options = {}) => fetchPet(url, options);
+
+    //peticiones
+
+    const post = (url, options) => {
+        options.method = "POST";
+        return fetchPet(url, options)
+    };
+
+    const put = (url, options = {}) => {
+        options.method = "PUT";
+        return fetchPet(url, options);
+    };  
+
+    const del = (url, options = {}) => {
+        options.method = "DELETE";
+        return fetchPet(url, options);
+    };
+
+    return{
+        get,
+        post,
+        put,
+        del,
+    };
+}
